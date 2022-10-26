523af286bd5926629f201da44e1a24b7ee377ea3
bhaas412 <bhaas412@gmail.com>
2022-10-26 12:18:27 -0400

    Fixed formatting issue with renderLicenseSection()

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -29,10 +29,9 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
     if (license !== 'None') {
-        return `
-        ## License
+        return `## License
         
-        This project is licensed under the ${license} license.`;
+This project is licensed under the ${license} license.`;
     }
     else {
         return '';
